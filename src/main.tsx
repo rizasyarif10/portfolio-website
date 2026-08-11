8ef8616cda20b41f0edccc0c24f2257f5f955204
Mochamad Riza Syarif <rizasyarif20@gmail.com>
2026-08-12 01:10:02 +0700

    fix config analytic vercel
    Signed-off-by: Mochamad Riza Syarif <rizasyarif20@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { inject } from "@vercel/analytics";
-import "maplibre-gl/dist/maplibre-gl.css";
-import "@/index.css";
+import { Analytics } from "@vercel/analytics/react";
+
 import App from "@/App";
 import { LanguageProvider } from "@/contexts/LanguageProvider";
-
-inject();
+import "@/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LanguageProvider>
       <App />
+      <Analytics />
     </LanguageProvider>
   </StrictMode>,
 );
